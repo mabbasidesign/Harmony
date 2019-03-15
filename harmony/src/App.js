dfe9426dc65d1014c78ad103e9061e7cdc77ca78
unknown <mabbasidesign@yahoo.com>
2019-03-15 09:47:42 +0330

    carrousel

--- harmony/src/App.js
+++ harmony/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './resources/styles.css';
 import Header from './components/header_footer/header'
+import Featured from './components/featured/index';
 
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{ height: "1500px" }}>
+      <div className="App" style={{ height: "1500px", backgroundColor: 'blue' }}>
          <Header />
+         <Featured />
       </div>
     );
   }
---
+++ harmony/src/components/featured/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Carrousel from './carrousel';
+
+const Featured = () => {
+
+    return (
+        <div style={{ position:'reletive' }}>
+
+        <Carrousel />
+
+        <div className="artist_name">
+            <div className="wrapper">
+                Harmony Festival
+            </div>
+        </div>
+
+        </div>
+    );
+}
+
+export default Featured;
