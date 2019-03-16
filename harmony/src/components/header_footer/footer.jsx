00c03983bbb66f4fedd40bfadc1ea34bed0a7af8
unknown <mabbasidesign@yahoo.com>
2019-03-16 10:28:28 +0330

    footer

--- harmony/src/components/header_footer/footer.jsx
+++ harmony/src/components/header_footer/footer.jsx
@@ -5,9 +5,9 @@ const Footer = () => {
     return (
         <footer className="bck_red">
             <Fade delay={500}>
-                <div className="font_righteous footer_logo_venue">The Venue</div>
+                <div className="font_righteous footer_logo_venue">Harmony</div>
                 <div className="footer_copyright">
-                    The venue 2018.All rights reserved.
+                    Harmony 2019.All rights reserved.
                 </div>
             </Fade>
         </footer>
